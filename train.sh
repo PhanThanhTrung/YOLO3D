8e71ebd076da0fb0827186960585b12845ffff85
PhanThanhTrung <trung99nb@gmail.com>
2022-07-05 16:16:29 +0700

    training script

---
+++ train.sh
@@ -0,0 +1,9 @@
+python train.py \
+    --epochs 100 \
+    --batch_size 32 \
+    --num_workers 4 \
+    --save_epoch 1 \
+    --train_path /root/nusc_kitti/training \
+    --model_path ./weights/ \
+    --select_model resnet18 \
+    --api_key xxx
